f552e075b31a3345916458e48378620fb246fa38
1802450038 <gabriel.chaus@gmail.com>
2025-10-10 09:59:38 -0300

    atualizando id para service order

--- app/Filament/Resources/ServiceOrderResource.php
+++ app/Filament/Resources/ServiceOrderResource.php
@@ -123,7 +123,15 @@ class ServiceOrderResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->defaultSort('id', 'desc')
+            ->poll('5s')
+            ->striped()
             ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->label('Código')
+                    ->badge()
+                    ->color('success')
+                    ->icon('heroicon-o-qr-code'),
                 Tables\Columns\TextColumn::make('user.name')
                     ->label('Registrado por')
                     ->numeric()
